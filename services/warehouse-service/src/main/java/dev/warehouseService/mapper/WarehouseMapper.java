f8b5b35e3808bfb7845e14be21c6de639479e49a
Rozhdestvenska, Valentyna <rozhdval@fel.cvut.cz>
2024-08-07 11:51:02 +0000

    JavaDoc for WarehouseMapper.java

--- services/warehouse-service/src/main/java/dev/warehouseService/mapper/WarehouseMapper.java
+++ services/warehouse-service/src/main/java/dev/warehouseService/mapper/WarehouseMapper.java
@@ -5,9 +5,27 @@ import dev.warehouseService.entity.Warehouse;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
+/**
+ * Mapper interface for converting between Warehouse entities and WarehouseDTO objects.
+ * Uses MapStruct for automatic mapping.
+ */
 @Mapper(componentModel = "spring")
 @Component
 public interface WarehouseMapper {
+
+    /**
+     * Converts a Warehouse entity to a WarehouseDTO.
+     *
+     * @param warehouse the Warehouse entity to be converted
+     * @return the converted WarehouseDTO
+     */
     WarehouseDTO warehouseToWarehouseDTO(Warehouse warehouse);
+
+    /**
+     * Converts a WarehouseDTO to a Warehouse entity.
+     *
+     * @param warehouseDTO the WarehouseDTO to be converted
+     * @return the converted Warehouse entity
+     */
     Warehouse warehouseDTOToWarehouse(WarehouseDTO warehouseDTO);
 }
