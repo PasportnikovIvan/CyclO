259a098714be62c25f29b9ac4bd09c42a56664c5
Rozhdestvenska, Valentyna <rozhdval@fel.cvut.cz>
2024-08-07 11:21:46 +0000

    javaDoc for MessageConsumer.java

--- services/rental-service/src/main/java/dev/rentalService/kafka/MessageConsumer.java
+++ services/rental-service/src/main/java/dev/rentalService/kafka/MessageConsumer.java
@@ -5,10 +5,19 @@ import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * Kafka message consumer for the rental service application.
+ * Listens for messages on specified Kafka topics and processes them.
+ */
 @Component
 public class MessageConsumer {
     private static final Logger logger = CustomLoggerFactory.getLogger(MessageConsumer.class);
-
+    
+    /**
+     * Listens for messages on the "warehouse-topic" Kafka topic and logs the received message.
+     *
+     * @param message the message received from the Kafka topic
+     */
     @KafkaListener(topics = "warehouse-topic", groupId = "rental-service-group")
     public void listenForWarehouseUpdates(String message) {
         logger.info("Received message from warehouse: {}", message);
