9d24849a4ed71bbd5174acf0e94c32e301a5318a
Rozhdestvenska, Valentyna <rozhdval@fel.cvut.cz>
2024-08-07 11:23:42 +0000

    JavaDoc for RentalMapper.java

--- services/rental-service/src/main/java/dev/rentalService/mapper/RentalMapper.java
+++ services/rental-service/src/main/java/dev/rentalService/mapper/RentalMapper.java
@@ -5,10 +5,28 @@ import dev.rentalService.entity.RentalRecord;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
+/**
+ * Mapper interface for converting between RentalRecord entities and RentalDTO objects.
+ * Uses MapStruct for automatic mapping.
+ */
 @Mapper(componentModel = "spring")
 @Component
 public interface RentalMapper {
+    
+    /**
+     * Converts a RentalRecord entity to a RentalDTO.
+     *
+     * @param rentalRecord the RentalRecord entity to be converted
+     * @return the converted RentalDTO
+     */
     RentalDTO rentalToRentalDTO(RentalRecord rentalRecord);
+
+    /**
+     * Converts a RentalDTO to a RentalRecord entity.
+     *
+     * @param rentalDTO the RentalDTO to be converted
+     * @return the converted RentalRecord entity
+     */
     RentalRecord rentalDTOToRental(RentalDTO rentalDTO);
 }
 
